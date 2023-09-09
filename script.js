a1d78a9b33742079ba3f51778760548327e800c4
Ada Rachel <108772486+adarachel@users.noreply.github.com>
2023-09-09 07:26:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-    function pad(number) {
-        if (number < 10) {
-            return "0" + number;
-        }
-        return number;
-    }
+const daysOfWeek = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+const currentDate = new Date();
+const currentDay = daysOfWeek[currentDate.getUTCDay()];
+document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent =
+  currentDay;
 
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const today = new Date();
-    const currentDay = daysOfWeek[today.getDay()];
-    const dayElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
-    dayElement.textContent = currentDay;
-
-    // Get UTC time
-    const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
-    const utcTime = pad(today.getUTCHours()) + ":" + pad(today.getUTCMinutes());
-    utcTimeElement.textContent = utcTime;
-});
+const currentUTCTime = Date.now();
+document.querySelector('[data-testid="currentUTCTime"]').textContent =
+  currentUTCTime;
